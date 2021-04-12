c537892ad6b17c1c948690ebce29fef5bcc686ba
Anastasiia Tsyrulnikova <ez53@rambler.ru>
2021-04-12 19:33:38 +0300

    git add .Задание 2

--- js/task-01.js
+++ js/task-01.js
@@ -11,22 +11,22 @@
     Количество вложенных li: 4
 */
 
-const items = document.querySelectorAll(".item");
-    Array.prototype.forEach.call(items, (element) => {
-    const title = element.querySelector('h2').innerHTML;
-    const itemsLength = element.querySelectorAll('li').length;
-    console.log(`Категория: ${title}.
-    Количество элементов: ${itemsLength}`);
-    })
+// const items = document.querySelectorAll(".item");
+//     Array.prototype.forEach.call(items, (element) => {
+//     const title = element.querySelector('h2').innerHTML;
+//     const itemsLength = element.querySelectorAll('li').length;
+//     console.log(`Категория: ${title}.
+//     Количество элементов: ${itemsLength}`);
+//     })
   
 // или:
 //  "use strict";
 
-// const totalCategories = document.querySelectorAll(".item");
-// console.log(`В списке ${totalCategories.length} категории.`);
+  const totalCategories = document.querySelectorAll(".item");
+  console.log(`В списке ${totalCategories.length} категории.`);
 
-// const categoriesArray = [...totalCategories]
-// .map(categories => `Категория: ${categories.children[0].textContent}
-// Количество элементов: ${categories.children[1].children.length}`)
-// .join("\n");
-// console.log(categoriesArray);
+  const categoriesArray = [...totalCategories]
+  .map(categories => `Категория: ${categories.children[0].textContent}
+  Количество элементов: ${categories.children[1].children.length}`)
+  .join("\n");
+  console.log(categoriesArray);
---
+++ js/task-02.js
@@ -0,0 +1,19 @@
+// "use strict";
+
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы"
+ ];
+
+    const listEl = document.querySelector('#ingredients');
+    const items = ingredients.map(item => {
+    const itemOfListEl = document.createElement('li');
+    itemOfListEl.textContent = item;
+    return itemOfListEl;
+  });
+  console.log(items);
+  listEl.append(...items);
